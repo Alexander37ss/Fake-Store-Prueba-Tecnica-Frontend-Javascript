0ac28b4d0d9c59ed42f4806f84da3b956e5f4019
Alexander37ss <alexanderpalazuelos37@gmail.com>
2024-07-08 21:21:03 -0700

    primer problema

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
+let storePagination = 5;
+
+localStorage.setItem('pagination', storePagination);
+
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
       let products = response;
-      let output = products.map(product => {
-        // template
-      });
+      let output = products.map(product => card(product));
       let newItem = document.createElement('section');
-      newItem.classList.add('Item');
+      newItem.classList.add('Items');
       newItem.innerHTML = output;
       $app.appendChild(newItem);
     })
@@ -19,13 +21,19 @@ const getData = api => {
 }
 
 const loadData = () => {
-  getData(API);
+  getData(`${API}${pagination(5)}`);
 }
-
+loadData();
 const intersectionObserver = new IntersectionObserver(entries => {
   // logic...
+  entries.forEach(entry => {
+    entry.target.classList.toggle('card', entry.isIntersecting);
+    if(entry.isIntersecting) intersectionObserver.unobserve(entry);
+  })
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
 
 intersectionObserver.observe($observe);
+
+loadData()
